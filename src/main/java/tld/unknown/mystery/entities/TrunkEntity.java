20921e957e6ffe28110a0819da81e1b2250740bf
Vatuu <21113232+Vatuu@users.noreply.github.com>
2022-11-12 14:31:58 +0100

    Jeremy Progress.

--- src/main/java/tld/unknown/mystery/entities/TrunkEntity.java
+++ src/main/java/tld/unknown/mystery/entities/TrunkEntity.java
@@ -1,19 +1,26 @@
 package tld.unknown.mystery.entities;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.animal.Wolf;
-import net.minecraft.world.entity.animal.axolotl.Axolotl;
-import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.MenuConstructor;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
+import tld.unknown.mystery.registries.ChaumtraftItems;
 
 import java.util.Optional;
 import java.util.Set;
@@ -21,12 +28,18 @@ import java.util.UUID;
 
 public class TrunkEntity extends Mob implements HasCustomInventoryScreen, OwnableEntity {
 
-    private final EntityDataAccessor<Byte> UPGRADES = SynchedEntityData.defineId(TrunkEntity.class, EntityDataSerializers.BYTE);
-    private final EntityDataAccessor<Boolean> STAY = SynchedEntityData.defineId(TrunkEntity.class, EntityDataSerializers.BOOLEAN);
-    private final EntityDataAccessor<Optional<UUID>> OWNER_UUID = SynchedEntityData.defineId(TrunkEntity.class, EntityDataSerializers.OPTIONAL_UUID);
+    private static final int SIZE_STANDARD = 27;
+    public static final int SIZE_UPGRADED = 36;
+
+    private static final EntityDataAccessor<Byte> UPGRADES = SynchedEntityData.defineId(TrunkEntity.class, EntityDataSerializers.BYTE);
+    private static final EntityDataAccessor<Boolean> STAY = SynchedEntityData.defineId(TrunkEntity.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Optional<UUID>> OWNER_UUID = SynchedEntityData.defineId(TrunkEntity.class, EntityDataSerializers.OPTIONAL_UUID);
+
+    private final SimpleContainer inventory;
 
     public TrunkEntity(EntityType<? extends Mob> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.inventory = new SimpleContainer(SIZE_STANDARD);
     }
 
     protected void registerGoals() {
@@ -34,19 +47,40 @@ public class TrunkEntity extends Mob implements HasCustomInventoryScreen, Ownabl
     }
 
     @Override
-    public void openCustomInventoryScreen(Player pPlayer) {
-
+    public void addAdditionalSaveData(CompoundTag pCompound) {
+        super.addAdditionalSaveData(pCompound);
+        pCompound.put("Contents", inventory.createTag());
     }
 
     @Override
-    public void addAdditionalSaveData(CompoundTag pCompound) {
-        super.addAdditionalSaveData(pCompound);
+    public void readAdditionalSaveData(CompoundTag pCompound) {
+        super.readAdditionalSaveData(pCompound);
+        inventory.fromTag(pCompound.getList("Contents", ListTag.TAG_COMPOUND));
+    }
+
+    @Override
+    protected InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+        this.openCustomInventoryScreen(pPlayer);
+        return InteractionResult.sidedSuccess(this.level.isClientSide);
+    }
+
+    @Override
+    public void openCustomInventoryScreen(Player pPlayer) {
+        if (!this.level.isClientSide()) {
+            MenuConstructor menu = (id, inv, p) -> isSizeUpgraded() ? new ChestMenu(MenuType.GENERIC_9x4, id, inv, inventory, 4) : ChestMenu.threeRows(id, inv, inventory);
+            pPlayer.getLevel().playSound(null, this, SoundEvents.CHEST_OPEN, SoundSource.NEUTRAL, 1, 1);
+            pPlayer.openMenu(new SimpleMenuProvider(menu, getTypeName()));
+        }
     }
 
     @Override
     public Entity getOwner() {
         UUID uuid = this.getOwnerUUID();
         return uuid == null ? null : this.level.getPlayerByUUID(uuid);
+    }
+
+    public boolean isSizeUpgraded() {
+        return ChaumtraftItems.UPGRADE_CAPACITY.get().isBitSet(getUpgradeByte());
     }
 
     /* -------------------------------------------------------------------------------------------------------------- */
@@ -58,6 +92,7 @@ public class TrunkEntity extends Mob implements HasCustomInventoryScreen, Ownabl
         super.defineSynchedData();
         getEntityData().define(UPGRADES, (byte)0);
         getEntityData().define(STAY, false);
+        getEntityData().define(OWNER_UUID, Optional.empty());
     }
 
     public byte getUpgradeByte() {
@@ -69,8 +104,12 @@ public class TrunkEntity extends Mob implements HasCustomInventoryScreen, Ownabl
         return getEntityData().get(OWNER_UUID).orElse(null);
     }
 
+    public boolean shouldSit() {
+        return getEntityData().get(STAY);
+    }
+
     /* -------------------------------------------------------------------------------------------------------------- */
-    /*                                           Living Entity Methods                                                */
+    /*                                             Mob Entity Methods                                                 */
     /* -------------------------------------------------------------------------------------------------------------- */
 
     @Override
